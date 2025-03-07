bc6be0d047f92bb3b742ff4b61cb4fda10fcb69c
hz-uiw <kwonmc5.11@gmail.com>
2025-03-07 22:30:17 +0900

    back setting

---
+++ src/main/java/com/korit/mcdonaldkiosk/McdonaldKioskApplication.java
@@ -0,0 +1,13 @@
+package com.korit.mcdonaldkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdonaldKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McdonaldKioskApplication.class, args);
+    }
+
+}
